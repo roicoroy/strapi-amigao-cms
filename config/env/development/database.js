3b5955fea66f61ef77aa84f532fb4cd6c6b83267
BentoRi <bentor@emea.cshare.net>
2022-05-03 15:51:28 +0100

    deploy!

--- config/env/development/database.js
+++ config/env/development/database.js
@@ -1,26 +1,15 @@
-const parse = require('pg-connection-string').parse;
-const config = parse(process.env.LOCAL_DATABASE_URL);
 module.exports = ({ env }) => ({
   connection: {
     client: 'postgres',
     connection: {
-      host: config.host,
-      port: config.port,
-      database: config.database,
-      user: config.user,
-      password: config.password,
-      ssl: {
-        rejectUnauthorized: false,
-        ssl: false
-      },
-    },
-    pool: {
-      max: 6,
-      min: 2,
-      idleTimeoutMillis: 30000,
-      createTimeoutMillis: 30000,
-      acquireTimeoutMillis: 30000,
-      propagateCreateError: false
+      host: env('DATABASE_HOST', 'localhost'),
+      port: env.int('DATABASE_PORT', 5432),
+      database: env('DATABASE_NAME', 'postgres'),
+      user: env('DATABASE_USERNAME', 'ricardobento'),
+      password: env('DATABASE_PASSWORD', '0000'),
+      schema: 'public',
+      rejectUnauthorized: false,
+      ssl: false
     },
     debug: true,
   },
