2825e2da9d14a477d9fb3b0a047247d84487a514
BentoRi <bentor@emea.cshare.net>
2022-05-04 22:42:00 +0100

    strapi new app

--- config/plugins.js
+++ config/plugins.js
@@ -1,5 +1,3 @@
-'use strict';
-
 module.exports = ({ env }) => ({
     documentation: {
         config: {
@@ -12,9 +10,9 @@ module.exports = ({ env }) => ({
         config: {
             provider: 'cloudinary',
             providerOptions: {
-                cloud_name: env('roicoroy'),
-                api_key: env('854797375527335'),
-                api_secret: env('3ELa56bTj-tjdmraafVDGon5p1M'),
+                cloud_name: env('CLOUDINARY_NAME'),
+                api_key: env('CLOUDINARY_API_KEY'),
+                api_secret: env('CLOUDINARY_API_SECRET'),
             },
         },
     },
