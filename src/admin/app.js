5babb2f2dba2fb0ba8ac1feb8d6116cc24f6b5fb
BentoRi <bentor@emea.cshare.net>
2022-05-23 09:35:52 +0100

    strapi new app

--- src/admin/app.js
+++ src/admin/app.js
@@ -1,35 +1,33 @@
+import theme from './extensions/theme';
+import head from './extensions/head';
+import menu from './extensions/menu';
+import Logo from 'assets/logo.svg';
+
 export default {
   config: {
-    locales: [
-      // 'ar',
-      // 'fr',
-      // 'cs',
-      // 'de',
-      // 'dk',
-      // 'es',
-      // 'he',
-      // 'id',
-      // 'it',
-      // 'ja',
-      // 'ko',
-      // 'ms',
-      // 'nl',
-      // 'no',
-      // 'pl',
-      // 'pt-BR',
-      // 'pt',
-      // 'ru',
-      // 'sk',
-      // 'sv',
-      // 'th',
-      // 'tr',
-      // 'uk',
-      // 'vi',
-      // 'zh-Hans',
-      // 'zh',
-    ],
+    auth: {
+      logo: Logo,
+    },
+    theme: {
+      colors: {
+        primary100: '#f6ecfc',
+        primary200: '#e0c1f4',
+        primary500: '#ac73e6',
+        primary600: '#9736e8',
+        primary700: '#8312d1',
+        danger700: '#b72b1a'
+      },
+    },
+    head: {
+      favicon:
+        Logo,
+      title: 'Strapi test',
+    },
+    menu: {
+      logo: Logo,
+    },
+    tutorials: false,
+    notifications: { release: false },
   },
-  bootstrap(app) {
-    console.log(app);
-  },
+  bootstrap() { },
 };
